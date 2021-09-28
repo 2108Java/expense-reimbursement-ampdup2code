ab8c796bef9575cefbfb3b23db030eeb162edab2
unknown <yibeltal.zerihun@revature.net>
2021-09-27 22:42:48 -0400

    Project 1 Startup

---
+++ src/main/java/com/ers/MainDriver.java
@@ -0,0 +1,21 @@
+package com.ers;
+
+import java.util.ArrayList;
+import java.util.List;
+import io.javalin.Javalin;
+import io.javalin.http.Context;
+
+public class MainDriver {
+	
+	public static void main(String[] args) {
+		
+		
+        Javalin app = Javalin.create().start(9000);
+        
+       // app.get("/User", ctx -> ctx.json(getUser(ctx)));
+        
+	
+	
+
+	}
+}

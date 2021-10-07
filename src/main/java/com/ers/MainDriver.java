a960082ee858dd45925bc162ff389728b1457edf
Jacky Thomas <mythology216@gmail.com>
2021-10-07 13:06:14 -0500

    adding Properties Parser

--- src/main/java/com/ers/MainDriver.java
+++ src/main/java/com/ers/MainDriver.java
@@ -5,12 +5,15 @@ import com.ers.controller.AuthenticateController;
 import com.ers.controller.RequestHandler;
 import com.ers.dao.DaoImp;
 import com.ers.service.AuthenticationSer;
+import com.ers.util.PropertiesParser;
 
 import io.javalin.Javalin;
 
 public class MainDriver {
 	
 	public static void main(String[] args) {
+		
+		PropertiesParser.getProperties();
 		
 		
 		Javalin app = Javalin.create(config -> config.addStaticFiles(

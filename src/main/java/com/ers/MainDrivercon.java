fd6f953458c675063e7949741525d45afaf41d78
unknown <yibeltal.zerihun@revature.net>
2021-10-07 12:47:43 -0400

    Login reimbursement

---
+++ src/main/java/com/ers/MainDrivercon.java
@@ -0,0 +1,33 @@
+package com.ers;
+//package com.ers;
+//
+//import com.ers.dao.DaoImp;
+//import com.ers.service.AuthenticationSer;
+//import com.ers.service.RegistrationSer;
+//import com.ers.service.ReimbursementSer;
+//
+//import Menu.Menu;
+//
+//public class MainDriver {
+//	
+//	public static void main(String[] args) {
+//
+//		
+//		DaoImp database = new DaoImp();
+//		
+//		AuthenticationSer service = new AuthenticationSer(database);
+//		RegistrationSer service1 = new RegistrationSer(database);
+//		ReimbursementSer service2 =new ReimbursementSer (database);
+//		//BankRegistrationService service1 =new BankRegistrationService(database);
+//		
+//		//TransactionProcessindServ service2 =new TransactionProcessindServ(database);
+//		
+//		//Menu mainMenu = new Menu(service,service1,service2);
+//		
+//		Menu mainMenu = new Menu(service,service1,service2);
+//		
+//		mainMenu.display();
+//		
+//	}
+//
+//}

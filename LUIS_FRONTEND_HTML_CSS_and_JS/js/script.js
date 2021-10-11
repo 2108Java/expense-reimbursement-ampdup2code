9dfad9a6b239ddb50c67eb63f2cda8d2d3d2c86c
Jacky Thomas <mythology216@gmail.com>
2021-10-11 12:13:52 -0500

    moving html folder into resources/public

--- LUIS_FRONTEND_HTML_CSS_and_JS/js/script.js
+++
@@ -1,8 +0,0 @@
-function displayRowCount(tableId) {
-    tableData = document.getElementById(tableId);
-    let totalNumOfRows = tableData.rows.length - 1;
-    console.log(totalNumOfRows);
-    return `You have ${totalNumOfRows} tickets open`;
-}
-
-document.getElementById("ticket-greeting").innerHTML = displayRowCount("e-reimbursement");
